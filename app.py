fd5ccfb613a5158f419790abf38250b0f708b11a
Fecheta Virgil-Vicentiu <virgilfecheta@gmail.com>
2022-03-29 22:41:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ import mysql.connector
 
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(
-    host="34.88.254.196",
-    user="root",
-    password="12345",
-    database="tema3-db",
-)
+# mydb = mysql.connector.connect(
+#     host="34.88.254.196",
+#     user="root",
+#     password="12345",
+#     database="tema3-db",
+# )
 
 
 @app.route('/')
 def index():
     # print(mydb)
 
-    mycursor = mydb.cursor()
+    # mycursor = mydb.cursor()
+    #
+    # mycursor.execute("SELECT * FROM translate")
+    #
+    # myresult = mycursor.fetchall()
 
-    mycursor.execute("SELECT * FROM translate")
-
-    myresult = mycursor.fetchall()
-
-    return render_template('index.html', mydb=myresult)
+    return render_template('index.html', mydb='okkkkk')
 
 
 @app.route('/translate/<value>')
