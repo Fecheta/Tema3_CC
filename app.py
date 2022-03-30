6e177fc12ee1001dc43c17c104453f5a8b394db4
Cristian Fartade <fartade.cristian@gmail.com>
2022-03-30 17:48:53 +0300

    change translate route

--- app.py
+++ app.py
@@ -18,7 +18,11 @@ app.config["RECAPTCHA_PRIVATE_KEY"] = "6LdPNysfAAAAAOTJN3IzODHa4VQVjfYdHrrB3c_l"
 
 @app.route('/')
 def index():
-    # configure Cloud SQL Python Connector properties
+    return render_template("index.html")
+
+
+@app.route('/translate/<value>')
+def translate(value):
     def getconn():
         conn = connector.connect(
             "cc-tema3-345518:europe-north1:database",
@@ -34,46 +38,18 @@ def index():
         creator=getconn,
     )
 
-    # query or insert into Cloud SQL database
-    with pool.connect() as db_conn:
-        # query database
-        result = db_conn.execute("SELECT * from translate").fetchall()
-
-        # Do something with the results
-        # for row in result:
-        #     print(row)
-
-    return render_template("index.html", data=result)
-
-
-@app.route('/translate2/<value>')
-def translate(value):
-    mydb = mysql.connector.connect(
-        host="34.88.254.196",
-        user="root",
-        password="12345",
-        database="tema3-db",
-    )
     result = translate_text('ro', value)
     original = value
     translated = result['translatedText']
     fromLanguage = result['detectedSourceLanguage']
     toLanguage = 'ro'
 
+    # query or insert into Cloud SQL database
+    with pool.connect() as db_conn:
+        # query database
+        query_result = db_conn.execute(f"INSERT INTO translate (original, translated, from_lang, to_lang) VALUES (\"{original}\", \"{translated}\", \"{fromLanguage}\", \"{toLanguage}\")")
 
-    mycursor = mydb.cursor()
-
-    mycursor.execute(f"INSERT INTO translate (original, translated, from_lang, to_lang) VALUES (\"{original}\", \"{translated}\", \"{fromLanguage}\", \"{toLanguage}\")")
-
-    mydb.commit()
-    mydb.close()
     return render_template('translate.html', result=result, db='Translation added to database')
-
-@app.route('/translate/<value>')
-def translate2(value):
-
-    return render_template('translate.html', result=translate_text('ro', value))
-
 
 
 @app.route('/gmail', methods=("GET", "POST"))
