b0d9f93138e8b136200fc3b7465ed2ed94016b23
martiscafilip <martisca_filip@yahoo.com>
2022-03-30 12:32:08 +0300

    reCAPTCHA added

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask, Response, jsonify, render_template
+from flask_wtf import FlaskForm, RecaptchaField
 import mysql.connector
+from wtforms import StringField, RadioField, SelectField, TextAreaField, DateTimeField, SubmitField
+from wtforms.validators import DataRequired
+
 from Gmail_Resources.gmail import sendmail
 from calendar_resources.Calendar import cal_requests
 from youtube.youtube_api import YoutubeAPI
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecretkey"
+app.config["RECAPTCHA_PUBLIC_KEY"] = "6LdPNysfAAAAAAcz55krT3CLz-RO6kdUCy5ay-kx"
+app.config["RECAPTCHA_PRIVATE_KEY"] = "6LdPNysfAAAAAOTJN3IzODHa4VQVjfYdHrrB3c_l"
 
+class Widgets(FlaskForm):
+    recaptcha = RecaptchaField()
+    name = StringField(label="Name", validators=[DataRequired()])
 
+    radio = RadioField(label="Please select Your Programming language ",
+                       choices=[('Python', "Python"), ["C++", "C++"]])
+
+    select = SelectField(label='select', choices=[("1", "WebApp"), ("2", "Web Scrapping")])
+    comments = TextAreaField(label="comments")
+    date = DateTimeField(label="Birthday", format='%Y-%m-%d')
+
+    submit = SubmitField(label="Submit")
 # @app.route('/')
 # def index():
 #     # # print(mydb)
@@ -37,12 +55,15 @@ def translate(value):
 
 @app.route('/gmail/<value>')
 def gmail(value):
+    print(value)
+    form = Widgets()
     sendmail()
-    return render_template('gmail.html', value=int(value))
+    return render_template('gmail.html', form=form)
 
 
 @app.route('/calendar')
 def calendar():
+
     str = cal_requests()
     return render_template('calendar.html', value=list(str))
 
